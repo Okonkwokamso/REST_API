ed388b0393906db4731c62764d997a61fdb979fd
Okonkwo Kamso <okonkwochikamsoj@gmail.com>
2024-05-28 12:08:24 +0100

    Setting up db connection

---
+++ src/utils/connect.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import config from 'config';
+import logger from './logger'
+
+const connect = async () => {
+  const dbUri = config.get('dbUri') as string;
+
+  try {
+    await mongoose.connect(dbUri);
+    logger.info('Database connected');
+  } catch (err) {
+    console.error(err);
+    logger.error('Database connection failed');
+    process.exit(1);
+    
+  }
+};
+
+export default connect;
+
+
+
+
+
+
+
+
